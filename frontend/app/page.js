1b542a384f941d69d217a181dd67ef64f689966a
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2025-01-04 13:31:52 +0000

    fixed fetch error

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -2,26 +2,23 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [message, setMesssage] = useState("");
-  const server = String(process.env.NEXT_SERVER_URL);
+  const [message, setMessage] = useState();
+  const server = process.env.NEXT_PUBLIC_SERVER_URL;
+
   useEffect(() => {
     if (!message) {
       fetch(server)
-        .then((data) => {
-          setMesssage(data.message);
-        })
-        .catch((err) => {
-          console.log("err", err);
-        });
+        .then((res) => res.json())
+        .then((data) => setMessage(data.message));
     }
-  });
+  }, []);
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold">
         LearnXcel : The Smart Learning Ecosystem App
       </h1>
       <br />
-      <p className="text-2xl">message:{message}</p>
+      <p className="text-2xl">message : {message}</p>
     </main>
   );
 }
